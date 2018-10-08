6e0b34b6f81e2124cb962aa260558722c2504e15
dennis <dennis@broesel-pc.fritz.box>
2018-10-08 20:31:03 +0200

    works

--- f429_player/App/Test.cpp
+++ f429_player/App/Test.cpp
@@ -8,8 +8,21 @@
 #include "Test.h"
 #include "stm32f4xx.h"
 #include "dac.h"
+#include "../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dac.h"
+
+void etrf(){
+
+}
+
+#ifdef __cplusplus
+	extern "C" {
+#endif
 
 void Start(){
 	HAL_GPIO_WritePin(LD_RED_GPIO_Port, LD_RED_Pin, GPIO_PIN_SET);
-	HAL_DAC_MODULE_ENABLED
+
 }
+
+#ifdef __cplusplus
+	}
+#endif
